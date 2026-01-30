b46ce072cc0b66b575e2a25ce8f1b6977f0b7968
Sahana S <4gw22cs097@gsss.edu.in>
2026-01-30 07:01:02 +0530

    Create App.js

---
+++ Existing System/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to the Learning Platform</h1>
+      <p>This project was created entirely inside GitHub!</p>
+    </div>
+  );
+}
